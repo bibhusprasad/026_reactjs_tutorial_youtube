c951e106202606f7fdf018850e3cc334b50c11b4
bibhusprasad <bibhusprasad@gmail.com>
2025-11-12 21:27:47 +0530

    01 First Sample react project

---
+++ react-blog/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h2>My First Application</h2>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ react-blog/src/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+    return (
+    <>
+        <div>
+            <h1>Welcome to the React Blog</h1>
+        </div>
+    </>
+    )
+}
+
+export default Header;
