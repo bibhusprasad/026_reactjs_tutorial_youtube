c06713659c7333663486098350ce1c0bdd2250f2
bibhusprasad <bibhusprasad@gmail.com>
2025-11-12 22:08:17 +0530

    02 component

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -6,8 +6,22 @@ function App() {
       <Header />
       <div>
         <h2>My First Application</h2>
+        <Fruit />
+        <Color />
       </div>
     </>
+  )
+}
+
+function Fruit() {
+  return(
+    <h3>3Apple</h3>
+  )
+}
+
+function Color() {
+  return(
+    <h3>Red</h3>
   )
 }
 
